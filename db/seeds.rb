6c970d472c72969da59a4c45c252b8915d902784
Daniel Landes <daniellandes@gmail.com>
2019-11-27 17:07:02 +0100

    v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([
+    { name: 'Ask Pizza', address: 'London', category: 'italian' },
+    { name: 'Eat Tokyo', address: 'Soho', category: 'japanese' },
+    { name: 'Yo Sushi', address: 'Paddington', category: 'japanese' },
+    { name: 'Belgo', address: 'Holborn', category: 'belgian' },
+    { name: 'Good Earth', address: 'Hampstead', category: 'chinese' }])
